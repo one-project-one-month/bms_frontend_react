7d874a5beb074821567a12bfb3dea39baa5b69fb
Amoz19 <aungmon10co@gmail.com>
2024-05-29 21:29:25 +0630

    Romove token

--- src/api-config.ts
+++ src/api-config.ts
@@ -11,12 +11,10 @@ Axios.defaults.baseURL = 'https://bms-backend-nodejs.vercel.app/api/v1';
 // need to check the jwt token from cookies and add it to the request header
 Axios.interceptors.request.use(
   (config) => {
-    // const token = Cookies.get('token');
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
-    // mock fn
-    config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbkNvZGUiOiJibXNfNGEwMGI3NzIwNWI1OGYyMzI0YTNkZTFhMWE5ZWNjM2MiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MTY5NzAxNzYsImV4cCI6MTcxNzA1NjU3Nn0.yBGJDjYb52zha2yh7HIDDp7YrW8vtujJ-hBsncsxiOA`;
+    const token = Cookies.get('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
