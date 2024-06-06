ea3b8dbfb66d281ca7eabf1d626b9d8409a6b01e
Aung <august810.a@gmail.com>
2024-06-06 20:34:19 +0700

    added useTransfer hook and fixed conflicts and transfer ui

---
+++ src/hooks/useTransfer.ts
@@ -0,0 +1,50 @@
+import { useMutation } from '@tanstack/react-query';
+import Axios from '../api-config';
+import { RequestBody,RequestBodySchema,ResponseData,ResponseDataSchema} from '../lib/types';
+
+const endpoints = {
+    transactions: 'https://bms-backend-nodejs.vercel.app/api/v1/admins/users/transactions',
+  };
+  
+  const submitTransaction = async (requestBody: RequestBody) => {
+    console.log(requestBody);
+    
+    RequestBodySchema.parse(requestBody);
+  
+    const response : ResponseData = await Axios.post(
+      endpoints.transactions,
+      requestBody,
+    );
+  
+    ResponseDataSchema.parse(response);
+  
+    return response;
+  };
+  
+  const useSubmitTransaction = () =>
+    useMutation({
+      mutationKey: ['submitTransaction'],
+      mutationFn: (requestBody: RequestBody) => {
+        return submitTransaction(requestBody);
+      },
+    //   onSuccess: (data) => {
+    //     console.log('Response data:', data);
+    //     setData(data.data);  // Ensure setData is properly defined in your component
+    //     setSuccess(true);  // Ensure setSuccess is properly defined in your component
+    //     setErrorMessage(null);  // Ensure setErrorMessage is properly defined in your component
+    //   },
+    //   onError: (error) => {
+    //     if (axios.isAxiosError(error)) {
+    //       console.error('Axios error:', error.message);
+    //       if (error.response) {
+    //         console.error('Response data:', error.response.data);
+    //         setErrorMessage(error.response.data);  // Ensure setErrorMessage is properly defined in your component
+    //         setSuccess(false);  // Ensure setSuccess is properly defined in your component
+    //       }
+    //     } else {
+    //       console.error('Unexpected error:', error);
+    //     }
+    //   },
+    });
+  
+  export default useSubmitTransaction;
--- src/pages/TransferPage.tsx
+++ src/pages/TransferPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import SuccessMessage from '../components/transfer/SuccessMessage';
 import TransferForm from '../components/transfer/TransferForm';
 import { ResponseData,ResponseDataSchema,RequestBody,RequestBodySchema,error } from '../lib/types';
+import useSubmitTransaction from '../hooks/useTransfer';
 
 const NotAllowed = () => (
     <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="24px" height="24px"><path fill="#f44336" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"/><path fill="#fff" d="M29.656,15.516l2.828,2.828l-14.14,14.14l-2.828-2.828L29.656,15.516z"/><path fill="#fff" d="M32.484,29.656l-2.828,2.828l-14.14-14.14l2.828-2.828L32.484,29.656z"/></svg>
@@ -22,7 +23,6 @@ const TransferPage = () => {
     const [success,setSuccess] = useState (false)
 
     const [errorMessage,setErrorMessage] = useState<error | null> (null)
-    console.log(errorMessage);
     
     // Define your request body
     const convertAmount = parseInt(accounts.amount.name,10)
@@ -59,44 +59,62 @@ const TransferPage = () => {
 
     const isCompleted = Object.values(accounts).every((value)=>value.name);  
 
-    const clickHandler = async()  => {
+    // const clickHandler = async()  => {
 
-        try {
-            // Validate the request body before making the request
-            RequestBodySchema.parse(requestBody);
+    //     try {
 
-            const response: AxiosResponse<{ data: ResponseData }> = await axios.post<{ data: ResponseData }>(
-                'https://bms-backend-nodejs.vercel.app/api/v1/admins/users/transactions',
-                requestBody,
-                config
-            );
-            // Validate the response data
-            ResponseDataSchema.parse(response.data.data);
+    //         RequestBodySchema.parse(requestBody);
+
+    //         const response: AxiosResponse<{ data: ResponseData }> = await axios.post<{ data: ResponseData }>(
+    //             'https://bms-backend-nodejs.vercel.app/api/v1/admins/users/transactions',
+    //             requestBody,
+    //             config
+    //         );
+
+    //         ResponseDataSchema.parse(response.data.data);
     
-            console.log('Response data:', response.data.data);
-            if(response.status == 200) {
-                setData(response.data.data)
-                setSuccess(true)
-                setErrorMessage(null)
-            }
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error('Axios error:', error.message);
-                if (error.response) {
-                    console.error('Response data:', error.response.data);
-                    setErrorMessage(error.response.data)
-                    setSuccess(false)
-                }
-            } else {
-                console.error('Unexpected error:', error);
-            }
-        }
+    //         console.log('Response data:', response.data.data);
+    //         if(response.status == 200) {
+    //             setData(response.data.data)
+    //             setSuccess(true)
+    //             setErrorMessage(null)
+    //         }
+    //     } catch (error) {
+    //         if (axios.isAxiosError(error)) {
+    //             console.error('Axios error:', error.message);
+    //             if (error.response) {
+    //                 console.error('Response data:', error.response.data);
+    //                 setErrorMessage(error.response.data)
+    //                 setSuccess(false)
+    //             }
+    //         } else {
+    //             console.error('Unexpected error:', error);
+    //         }
+    //     }
+    // }
+
+    const submitTransaction = useSubmitTransaction()
+
+    const clickHandler = () => {
+        submitTransaction.mutate(requestBody)
     }
+
+    useEffect(() => {
+        if (submitTransaction.isSuccess && submitTransaction.data) {
+          setData(submitTransaction.data);
+        } else if (submitTransaction.isError) {
+          console.log(submitTransaction.error);
+        }
+      }, [
+        submitTransaction.isError,
+        submitTransaction.isSuccess,
+        submitTransaction.isPending,
+      ]);
 
     return (
         <div className="w-full mx-auto h-screen">
-            <div className={`w-full max-w-lg ${success || errorMessage?.message ? 'mx-auto mt-20' : 'ml-4 mt-8'}`}>
-                <form className="bg-secondaryBg border border-borderColor rounded-md px-8 pt-6 pb-8 mb-4">
+            <div className={`w-full ${success || errorMessage?.message ? 'mx-auto mt-20' : 'ml-4 mt-8'}`}>
+                <form className="bg-secondaryBg border border-borderColor rounded-md px-8 pt-6 pb-6">
                         {!success && !errorMessage?.message && <TransferForm accounts={accounts} handleOnChange={handleOnChange} 
                         isCompleted={isCompleted} clickHandler={clickHandler}/>}
 
