c4679dd8aaf03f6ebff69349b0808e4971ebdd78
Amoz19 <aungmon10co@gmail.com>
2024-06-08 22:15:52 +0630

    Fix userData fetching

--- src/hooks/useUserList.ts
+++ src/hooks/useUserList.ts
@@ -1,16 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import Axios from "../api-config";
-import { UserListResponse } from "@/lib/types";
+import { useQuery } from '@tanstack/react-query';
+import Axios from '../api-config';
+import { UserListResponse } from '@/lib/types';
 
 const getUsers = async () => {
-    const response = await Axios.get<UserListResponse>('/users');
-    
-    return response
-}
+  const response = await Axios.get<UserListResponse>('/users');
+  return response;
+};
 
 export const useUserAccounts = () => {
-    return useQuery({
-        queryKey: ['users'],
-        queryFn: getUsers,
-    })
-}
+  return useQuery({
+    queryKey: ['users'],
+    queryFn: getUsers,
+  });
+};
