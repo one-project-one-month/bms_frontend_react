2ca80a5a866c37429f551ce4a5de3b12b2a2e319
Amoz19 <aungmon10co@gmail.com>
2024-06-12 20:28:53 +0630

    Add back button in transaction detail

--- src/components/TransactionHistory/TransactionDetails.tsx
+++ src/components/TransactionHistory/TransactionDetails.tsx
@@ -1,16 +1,30 @@
 
-import { useLocation } from 'react-router'
+import { useLocation, useNavigate } from 'react-router'
 import TransferSuccess from '../transfer/SuccessMessage'
+import { ArrowLeft } from 'lucide-react'
+
 
 
 const TransactionDetails = () => {
     const location = useLocation()
+    const navigate = useNavigate()
     const { data } = location.state
 
     return (
-        <div className='flex m-auto  w-[400px] h-[80vh] shadow px-8 '>
-            <TransferSuccess data={data} />
-        </div>
+        <>
+            <div
+                className='mx-16 mt-8 w-fit px-2 flex items-center bg-primaryBtn text-white rounded-2xl cursor-pointer'
+                onClick={() => navigate("..")}
+            >
+                <ArrowLeft size={18} />
+                <p>Back</p>
+            </div>
+            <div className='flex m-auto  w-[400px] h-[60vh] shadow px-8 '>
+
+                <TransferSuccess data={data} />
+            </div>
+        </>
+
 
     )
 }
